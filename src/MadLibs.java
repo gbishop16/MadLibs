9b3aa5837924c9c2d98c1cbb3e2ec1ff179a8935
gbishop16 <gqbishop@gmail.com>
2020-09-02 09:37:32 -0700

    Revert "updating buffered reader"
    This reverts commit e6ff35522e5a11b8409221a41391366b49eec7f9.

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -7,14 +6,14 @@ public class MadLibs
 {
 	
 	//0. Give variable names for each of the arraylists below and create Madlibs stories.
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args)
 	{
-		
+	try {	
 	//1. Read a nouns.txt file and store its list of nouns into an arraylist.
-	
-	BufferedReader br = new BufferedReader(new FileReader("nouns.txt"));
+	FileReader fr = new FileReader("nouns.txt");
+	BufferedReader br = new BufferedReader(fr);
 	//2. Read a verbs.txt file and store its list of verbs into an arraylist.
-	
+	}catch(IOException e) {}
 	}
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 }
