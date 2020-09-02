59b5877d1ad8d7a7b2054d439b2c5554433fcf91
samanthamcloughlin <62968552+smcloughlin13@users.noreply.github.com>
2020-09-02 09:12:39 -0700

    addVerbs
    adds the verbs, assuming there is one per line

--- MadLibs.java
+++ MadLibs.java
@@ -1,15 +1,46 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MadLibs {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
 		
 		//1. Read a nouns.txt file and store its list of nouns into an arraylist.
-		BufferedReader brd = new BufferedReader(new FileReader("nouns.txt"));
+		BufferedReader br = new BufferedReader(new FileReader("nouns.txt"));
 		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
+		
+		BufferedReader brVerbs = new BufferedReader(new FileReader("Verbs.txt"));
+		
+		ArrayList <String> verbs = new ArrayList <String>( );
+		
+			//read through the file
+		String newVerb = "";
+		
+		while (brVerbs.ready() == true)
+		{
+			//newVerb indicates the current word we're reading in
+			char next = (char)brVerbs.read();
+			
+			//if the next character read is a space, add newVerb to the list + reset it
+			if ((int)next == 10)
+			{
+				verbs.add(newVerb);
+				newVerb = "";
+			}
+			
+			//otherwise, add the character to newVerb
+			else
+			{
+				newVerb+=next;
+			}
+		}
+		brVerbs.close();
+		
+		System.out.println (verbs);
 		
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 		
