e6ff35522e5a11b8409221a41391366b49eec7f9
gbishop16 <gqbishop@gmail.com>
2020-09-02 08:30:44 -0700

    updating buffered reader
    made reader more efficient and added mroe nouns.

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -6,14 +7,14 @@ public class MadLibs
 {
 	
 	//0. Give variable names for each of the arraylists below and create Madlibs stories.
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-	try {	
+		
 	//1. Read a nouns.txt file and store its list of nouns into an arraylist.
-	FileReader fr = new FileReader("nouns.txt");
-	BufferedReader br = new BufferedReader(fr);
+	
+	BufferedReader br = new BufferedReader(new FileReader("nouns.txt"));
 	//2. Read a verbs.txt file and store its list of verbs into an arraylist.
-	}catch(IOException e) {}
+	
 	}
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 }
