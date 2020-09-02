9f1b89ef793c9cc649a88091d9e5ae01c632f683
gbishop16 <gqbishop@gmail.com>
2020-09-02 09:20:55 -0700

    addNouns
    finishes the addNouns and adds more nouns to the file. creates an arraylist for the nouns to be used in madLibs.

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -2,17 +2,31 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MadLibs
 {
 	
 	//0. Give variable names for each of the arraylists below and create Madlibs stories.
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args) throws IOException
 	{
 		
 	//1. Read a nouns.txt file and store its list of nouns into an arraylist.
 	
 	BufferedReader br = new BufferedReader(new FileReader("nouns.txt"));
+	ArrayList nouns = new ArrayList<String>();
+	while(br.ready())
+	{
+		for(int i = 0; i<"nouns.txt".length()-1; i++)
+		{
+			
+			nouns.add(br.readLine());
+			
+		}
+	}
+	br.close();
+	
+	System.out.print(nouns);
 	//2. Read a verbs.txt file and store its list of verbs into an arraylist.
 	
 	}
