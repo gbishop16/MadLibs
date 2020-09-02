f138c0a6d11d504f1097fe5320a8966dff05efdb
gbishop16 <gqbishop@gmail.com>
2020-09-02 09:39:06 -0700

    Revert "filereader"
    This reverts commit 4be4da01462fd847646ce26c2b6cdb41de9d5042.

--- MadLibs.java
+++ MadLibs.java
@@ -1,14 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 public class MadLibs {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
 		
 		//1. Read a nouns.txt file and store its list of nouns into an arraylist.
-		BufferedReader brd = new BufferedReader(new FileReader("nouns.txt"));
+		
 		//2. Read a verbs.txt file and store its list of verbs into an arraylist.
 		
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
